fd6de131fdfef654e5df4941a59dbbc52c242141
acrypthash <35197067+acrypthash@users.noreply.github.com>
2023-01-19 15:54:40 -0500

    Add files via upload

---
+++ EPSS_API_Access.py
@@ -0,0 +1,30 @@
+
+import json
+import requests
+import pandas
+import csv
+
+
+#EPSS API URL
+#url = 'https://api.first.org/data/v1/epss?envelope=true&pretty=true'
+url = 'https://api.first.org/data/v1/epss?order=!epss'
+
+
+#Datasets pulled from the API
+headers = {
+ 'data'
+ 'Content-Type': 'application/json; charset=utf-8'
+ }
+
+#API Request, conversion to json, formatting to pandas table, prints table
+r = requests.get(url, headers=headers)
+
+#panda stuff that was used for reviewing in terminal
+files = r.json()
+df = pandas.DataFrame(files)
+
+#panada exports results to CSV
+df.to_csv("EPSSresults.csv")
+#print(df.to_string())
+
+
